f998355b7fa4b8f09748b6c45f34f4ae7cc2b530
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2026-01-28 18:22:49 +0200

    Update main.dart and pubspec.yaml to enhance app functionality and dependencies

--- android/app/src/main/kotlin/com/example/screen_shot/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/screen_shot/MainActivity.kt
@@ -1,14 +1,84 @@
 package com.example.screen_shot
 
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
+import android.content.Context
+import android.hardware.display.DisplayManager
+import android.util.Log
+import android.view.SurfaceView
+import android.view.View
+import android.view.ViewGroup
 import android.view.WindowManager.LayoutParams
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterFragmentActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        window.addFlags(LayoutParams.FLAG_SECURE)
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity : FlutterFragmentActivity() {
+    private val CHANNEL = "screenNumbers"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+        window.addFlags(LayoutParams.FLAG_SECURE)
+
+        if (!setSecureSurfaceView()) {
+            Log.e("MainActivity", "Could not secure the MainActivity!")
+            // React as appropriate.
+        }
+//        SurfaceView(applicationContext).setSecure(true) // This line is likely redundant or misplaced.
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            // Note: this method is invoked on the main thread.
+                call, result ->
+            if (call.method == "getScreensNumber") {
+                val screensManager = getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+                val screenNumbers = screensManager.displays.size
+
+                if (screenNumbers >= 0) { // Check if screenNumbers is non-negative
+                    result.success(screenNumbers)
+                } else {
+                    result.error("UNAVAILABLE", "Screen numbers not available.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+
+    private fun setSecureSurfaceView(): Boolean {
+        val content = findViewById<ViewGroup>(android.R.id.content)
+        if (!isNonEmptyContainer(content)) {
+            Log.w("MainActivity", "Content view is not a non-empty container.")
+            return false
+        }
+        val splashView = content.getChildAt(0)
+        if (!isNonEmptyContainer(splashView)) {
+            Log.w("MainActivity", "Splash view is not a non-empty container.")
+            return false
+        }
+        val flutterView = (splashView as ViewGroup).getChildAt(0)
+        if (!isNonEmptyContainer(flutterView)) {
+            Log.w("MainActivity", "Flutter view is not a non-empty container.")
+            return false
+        }
+        // Iterate through children of flutterView to find the SurfaceView
+        for (i in 0 until (flutterView as ViewGroup).childCount) {
+            val child = flutterView.getChildAt(i)
+            if (child is SurfaceView) {
+                child.setSecure(true)
+                this.window.setFlags(LayoutParams.FLAG_SECURE, LayoutParams.FLAG_SECURE)
+                Log.i("MainActivity", "SurfaceView secured successfully.")
+                return true
+            }
+        }
+        Log.e("MainActivity", "SurfaceView not found within FlutterView.")
+        return false
+    }
+
+    private fun isNonEmptyContainer(view: View?): Boolean {
+        return view is ViewGroup && view.childCount > 0
     }
 }
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,27 +1,118 @@
 import UIKit
 import Flutter
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
+    weak var screen : UIView? = nil
+    var overlayController = UIViewController()
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         GeneratedPluginRegistrant.register(with: self)
-        self.window.makeSecure()
+        if let window = self.window {
+            window.makeSecure()
+        } else if #available(iOS 13.0, *) {
+            if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+               let window = scene.windows.first {
+                window.makeSecure()
+            }
+        }
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(screenRecordingStatusChanged),
+            name: UIScreen.capturedDidChangeNotification,
+            object: nil
+        )
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(screenshotHasTaken),
+            name: UIApplication.userDidTakeScreenshotNotification,
+            object: nil
+        )
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            if UIScreen.main.isCaptured {
+                self.displayOverlayControllerWith(
+                    message: "Screen recording is not allowed while using the app. Kindly turn off the screen recording to continue using the app."
+                )
+            }
+        }
+
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    override func applicationWillResignActive(_ application: UIApplication) {
+        self.blurScreen()
+    }
+
+    override func applicationDidBecomeActive(_ application: UIApplication) {
+        self.removeBlurScreen()
+    }
+
+    @objc func screenRecordingStatusChanged() {
+        if UIScreen.main.isCaptured {
+            self.displayOverlayControllerWith(
+                message: "Screen recording is not allowed while using the app. Kindly turn off the screen recording to continue using the app."
+            )
+        } else {
+            self.overlayController.dismiss(animated: false, completion: nil)
+        }
+    }
+
+    @objc func screenshotHasTaken() {}
+
+    fileprivate func displayOverlayControllerWith(message : String) {
+        if UIApplication.shared.windows.count > 0 {
+            let rootWindow = UIApplication.shared.windows[0]
+            self.overlayController.view.backgroundColor = .white
+            self.overlayController.modalPresentationStyle = .fullScreen
+
+            let screenWidth = UIScreen.main.bounds.width
+            let screenHeight = UIScreen.main.bounds.height - (rootWindow.safeAreaInsets.top + rootWindow.safeAreaInsets.bottom)
+            let frameOfLabel = CGRect(x: 20, y: screenHeight/2 - 100, width: screenWidth - 40, height: 200)
+
+            if let labelMessage = self.overlayController.view.viewWithTag(1010) as? UILabel {
+                labelMessage.text = message
+            } else {
+                let labelMessage = UILabel(frame: frameOfLabel)
+                labelMessage.tag = 1010
+                labelMessage.numberOfLines = 0
+                labelMessage.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+                labelMessage.text = message
+                labelMessage.textColor = .black
+                labelMessage.textAlignment = .center
+                self.overlayController.view.addSubview(labelMessage)
+            }
+
+            rootWindow.rootViewController?.present(self.overlayController, animated: false, completion: nil)
+        }
+    }
+
+    func blurScreen(style: UIBlurEffect.Style = .regular) {
+        screen = UIScreen.main.snapshotView(afterScreenUpdates: false)
+        let blurEffect = UIBlurEffect(style: style)
+        let blurBackground = UIVisualEffectView(effect: blurEffect)
+        screen?.addSubview(blurBackground)
+        blurBackground.frame = (screen?.frame)!
+        window?.addSubview(screen!)
+    }
+
+    func removeBlurScreen() {
+        screen?.removeFromSuperview()
     }
 }
 
-// this extension will shaw black screen
 extension UIWindow {
-func makeSecure() {
-    let field = UITextField()
-    field.isSecureTextEntry = true
-    self.addSubview(field)
-    field.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-    field.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-    self.layer.superlayer?.addSublayer(field.layer)
-    field.layer.sublayers?.first?.addSublayer(self.layer)
-  }
+    func makeSecure() {
+        let field = UITextField()
+        field.isSecureTextEntry = true
+        self.addSubview(field)
+        field.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        field.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        self.layer.superlayer?.addSublayer(field.layer)
+        field.layer.sublayers?.last?.addSublayer(self.layer)
+    }
 }
