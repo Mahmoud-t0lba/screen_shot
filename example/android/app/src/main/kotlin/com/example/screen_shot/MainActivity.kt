e572c9476012b121df26bb6342d3543637586f0b
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2026-01-28 19:34:54 +0200

    Convert project to prevent_screenshot_io plugin

--- example/android/app/src/main/kotlin/com/example/screen_shot/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/screen_shot/MainActivity.kt
@@ -1,51 +1,6 @@
 package com.example.screen_shot
 
-import android.content.Context
-import android.hardware.display.DisplayManager
-import android.util.Log
-import android.view.SurfaceView
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager.LayoutParams
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
 
-import androidx.annotation.NonNull
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-class MainActivity : FlutterFragmentActivity() {
-    private val CHANNEL = "prevent_screen"
-
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-
-        MethodChannel(
-            flutterEngine.dartExecutor.binaryMessenger,
-            CHANNEL
-        ).setMethodCallHandler { call, result ->
-            when (call.method) {
-                "enableSecure" -> {
-                    window.addFlags(LayoutParams.FLAG_SECURE)
-                    result.success(true)
-                }
-
-                "disableSecure" -> {
-                    window.clearFlags(LayoutParams.FLAG_SECURE)
-                    result.success(true)
-                }
-
-                "getScreensNumber" -> {
-                    val screensManager = getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
-                    val screenNumbers = screensManager.displays.size
-                    result.success(screenNumbers)
-                }
-
-                else -> {
-                    result.notImplemented()
-                }
-            }
-        }
-    }
+class MainActivity: FlutterActivity() {
 }
--- ios/Classes/PreventScreenshotIoPlugin.swift
+++ ios/Classes/PreventScreenshotIoPlugin.swift
@@ -2,32 +2,61 @@ import Flutter
 import UIKit
 
 public class PreventScreenshotIoPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "prevent_screenshot_io", binaryMessenger: registrar.messenger())
-    let instance = PreventScreenshotIoPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "enableSecure":
-      if let window = UIApplication.shared.windows.first {
-        window.makeSecure()
-        result(nil)
-      } else {
-        result(FlutterError(code: "UNAVAILABLE", message: "No window found", details: nil))
-      }
-    case "disableSecure":
-      if let window = UIApplication.shared.windows.first {
-        window.makeInsecure()
-        result(nil)
-      } else {
-        result(FlutterError(code: "UNAVAILABLE", message: "No window found", details: nil))
-      }
-    default:
-      result(FlutterMethodNotImplemented)
+    private var channel: FlutterMethodChannel?
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "prevent_screenshot_io", binaryMessenger: registrar.messenger())
+        let instance = PreventScreenshotIoPlugin()
+        instance.channel = channel
+        registrar.addMethodCallDelegate(instance, channel: channel)
+        registrar.addApplicationDelegate(instance)
     }
-  }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "enableSecure":
+            if let window = UIApplication.shared.windows.first {
+                window.makeSecure()
+                result(nil)
+            } else {
+                result(FlutterError(code: "UNAVAILABLE", message: "No window found", details: nil))
+            }
+        case "disableSecure":
+            if let window = UIApplication.shared.windows.first {
+                window.makeInsecure()
+                result(nil)
+            } else {
+                result(FlutterError(code: "UNAVAILABLE", message: "No window found", details: nil))
+            }
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    // Auto-blur implementation for the App Switcher
+    public func applicationWillResignActive(_ application: UIApplication) {
+        if let window = UIApplication.shared.windows.first {
+            blurScreen(in: window)
+        }
+    }
+
+    public func applicationDidBecomeActive(_ application: UIApplication) {
+        if let window = UIApplication.shared.windows.first {
+            removeBlurScreen(from: window)
+        }
+    }
+
+    private func blurScreen(in window: UIWindow) {
+        let blurEffect = UIBlurEffect(style: .regular)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = window.bounds
+        blurEffectView.tag = 9998
+        window.addSubview(blurEffectView)
+    }
+
+    private func removeBlurScreen(from window: UIWindow) {
+        window.viewWithTag(9998)?.removeFromSuperview()
+    }
 }
 
 extension UIWindow {
